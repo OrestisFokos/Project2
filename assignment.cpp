437be4d90ad3b023f7de7224d05d6af9144ba06b
AkisBon <anonimostheone@gmail.com>
2019-12-05 09:57:50 +0200

    k-means++ compile?

--- assignment.cpp
+++ assignment.cpp
@@ -23,3 +23,37 @@ void lloyds_assignment(  vector <vector<new_type> > *points, vector <vector<new_
     }
   }
 }
+
+void Update(vector<vector<new_type>> cluster, vector<new_type>* centroids){ // need *centroid so i can update inside function
+    vector<new_type> new_centroid;
+    int cluster_size = cluster.size();
+    for (int i = 0; i < cluster_size; i++){
+        for (int j = 0; j < cluster[i].size(); j++){
+          if (i == 0){
+            new_centroid.push_back(cluster[i][j]);
+          }
+          else{
+            new_centroid[j] += cluster[i][j];
+          }
+        }
+    }
+
+    for (int i = 0; i < new_centroid.size(); i++){
+      centroids->at(i) = new_centroid[i] / (double) cluster_size;
+    }
+}
+
+//arg.x is the point that we are searching for its cluster
+//find closest centroid so we know x's cluster
+int findCluster(vector <vector<new_type>>* centroids, vector<new_type> x){
+  int min_i = 0;
+  double min_dist =1000000.0;//a big number
+  for (int i = 0; i < centroids->size(); i++){
+    double dist = distance(x,centroids->at(i),"manhattan");
+    if (min_dist < dist){
+      min_dist = dist;
+      min_i = i;
+    }
+  }
+  return min_i; //cluster is in position min_i
+}
--- assignment.h
+++ assignment.h
@@ -7,6 +7,9 @@ typedef double new_type;
 //range search , LSH (inverse assignment - slide 44)
 
 void lloyds_assignment(  vector <vector<new_type> > * points, vector <vector<new_type> > * centroids);
+void Update(vector<vector<new_type>> cluster, vector<new_type>* centroids);
+
+int findCluster(vector <vector<new_type>>* centroids, vector<new_type> x);
 //na ta kanw pointers??
 
 #endif //ASSIGNMENT_H
