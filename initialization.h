c1a6720232de9e1b2f86f46807999010791de045
OrestisFokos <orestis.fokos@hotmail.com>
2019-12-06 12:27:00 +0200

    renamed files: random_initialization to initialization

---
+++ initialization.h
@@ -0,0 +1,11 @@
+#ifndef INITIALIZATION_H
+#define INITIALIZATION_H
+
+vector <vector<new_type> *> * random_initialization (
+  vector<vector<new_type>>  * Dataset,
+  int K);
+
+
+
+
+#endif //INITIALIZATION_H
